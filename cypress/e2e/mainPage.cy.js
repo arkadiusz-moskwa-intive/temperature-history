c07474c51f70c47021c9ada50bc2995cbae0c9cf
Arkadiusz Moskwa <arkadiusz.moskwa@intive.com>
2023-12-06 10:12:42 +0100

    Provide simple e2e test

--- cypress/e2e/mainPage.cy.js
+++ cypress/e2e/mainPage.cy.js
@@ -1,5 +1,35 @@
 describe('Visiting the main page', () => {
+  before(() => {
+    cy.setLocalStorage('temperature-store-Warsaw', JSON.stringify([
+      {
+        temperature: -4,
+        id: 1701853024063,
+      },
+      {
+        temperature: -3,
+        id: 1701853034063
+      },
+      {
+        temperature: -5,
+        id: 1701853044063
+      },
+      {
+        temperature: -10,
+        id: 1701853054063
+      },
+      {
+        temperature: -20,
+        id: 1701853064063
+      },
+    ]))
+  });
+  beforeEach(() => {
+    cy.visit('/');
+  });
   it('checks whether all important elements are present', () => {
-    cy.visit('/')
+    cy.get('h1').should('contain.text', 'Temperature in Warsaw');
+    cy.get('h2').should('contain.text', 'Current temperature:');
+    cy.get('#temperature-data tr').should('have.length.above', 5);
+    cy.get('#temperature-chart').should('have.length', 1);
   })
 })
